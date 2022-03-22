d9274890eed508fd5e19af6ef10846c214238891
Mikhailov <mhlw@yandex.ru>
2022-03-22 23:40:14 +0300

    Add frontend page with ajax.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import uvicorn
-from routers import mainpage_router
+from starlette.middleware.cors import CORSMiddleware
 
+from routers import mainpage_router
 
 if __name__ == "__main__":
     app = mainpage_router.app
+    origins = ["*"]
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     uvicorn.run(app, host="127.0.0.1", port=8000)
--- routers/mainpage_router.py
+++ routers/mainpage_router.py
@@ -11,6 +11,7 @@ SQLALCHEMY_DATABASE_URL = (
 )
 
 app = FastAPI()
+barriers = list()
 
 
 @app.on_event("startup")
@@ -23,20 +24,39 @@ async def shutdown():
     await database.database.disconnect()
 
 
+# пока по id, потом по номеру
+async def get_barrier(barrier_id: int):
+    query = (
+        select(
+            [
+                basicModels.barriers_table.c.id,
+                basicModels.barriers_table.c.number,
+                basicModels.barriers_table.c.description
+            ]
+        ).where(basicModels.barriers_table.c.id == barrier_id)
+    )
+    res = await database.database.fetch_all(query)
+    fetched_barriers = list()
+    for row in res:
+        fetched_barriers.append([row[0], row[1], row[2]])
+    return fetched_barriers[0]
+
+
 async def get_all_barriers():
     query = (
         select(
             [
+                basicModels.barriers_table.c.id,
                 basicModels.barriers_table.c.number,
                 basicModels.barriers_table.c.description
             ]
         )
     )
     res = await database.database.fetch_all(query)
-    barriers = list()
+    fetched_barriers = list()
     for row in res:
-        barriers.append([row[0], row[1]])
-    return barriers
+        fetched_barriers.append([row[0], row[1], row[2]])
+    return fetched_barriers
 
 
 @app.get("/")
@@ -46,7 +66,26 @@ async def read_root(request: Request):
     return templates.TemplateResponse("pages/mainpage.html", {"request": request, "content": content})
 
 
-@app.get("/incoming_call/")
-async def add_page_to_dashboard(number: str):
-    # ???
-    app.get("/")
+@app.get("/main")
+async def add_page_to_dashboard(request: Request):
+    content = dict()
+    content['barriers_list'] = await get_all_barriers()
+    return templates.TemplateResponse("pages/dynamic_data.html", {"request": request, "content": content})
+
+
+# пока по id, потом по номеру
+@app.post("/incoming_call/")
+async def add_barrier_to_list(barrier_id: int):
+    filtred = list(filter(lambda x: x["id"] == barrier_id, barriers))
+    if len(filtred) > 0:
+        while filtred[0] in barriers:
+            barriers.remove(filtred[0])
+    else:
+        barrier = await get_barrier(barrier_id)
+        barriers.append({"id": barrier[0], "number": barrier[1], "description": barrier[2]})
+    return {"id": barrier[0], "number": barrier[1], "description": barrier[2]}
+
+
+@app.get("/incoming_calls")
+async def get_current_incoming_calls(request: Request):
+    return barriers
